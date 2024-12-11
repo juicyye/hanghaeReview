cad8f83271763511338b0b910afb154d50483665
param <spsummerdoc@gmail.com>
2024-12-11 16:41:52 +0900

    test(productMetricsService): 리뷰 조회수, 평균 점수 업데이트 테스트 로직 추가

---
+++ src/test/java/hanghae/review/shop/product/service/ProductMetricsServiceTest.java
@@ -0,0 +1,68 @@
+package hanghae.review.shop.product.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import hanghae.review.mock.FakeProductRepository;
+import hanghae.review.mock.FakeReviewRepository;
+import hanghae.review.shop.product.domain.Product;
+import hanghae.review.shop.review.domain.Review;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ProductMetricsServiceTest {
+
+    private ProductMetricsService productMetricsService;
+    private FakeProductRepository productRepository;
+
+    @BeforeEach
+    void setUp() {
+        productRepository = new FakeProductRepository();
+        FakeReviewRepository reviewRepository = new FakeReviewRepository();
+        productMetricsService = new ProductMetricsService(productRepository, reviewRepository);
+
+        Product product = createProduct(2.2f, 3L);
+        productRepository.save(product);
+        reviewRepository.save(createReview(product, 3.5f, 1L));
+    }
+
+    @Test
+    @DisplayName("리뷰의 개수와 평균 점수를 알 수 있다")
+    void updateReviewMetrics() throws Exception {
+        // given
+        Long productId = 1L;
+        Float score = 3.0f;
+
+        // when
+        productMetricsService.updateReviewMetrics(productId, score);
+        Product result = productRepository.findById(productId).get();
+
+        // then
+        assertAll(() -> {
+            assertThat(result.getScore()).isEqualTo((score + 3.5f) / 2);
+            assertThat(result.getReviewCount()).isEqualTo(2);
+        });
+    }
+
+    private Product createProduct(float score, long reviewCount) {
+        return Product.builder()
+                .id(1L)
+                .score(score)
+                .reviewCount(reviewCount)
+                .build();
+    }
+
+    private Review createReview(Product product, float score, long userId) {
+        return Review.builder()
+                .content("내용")
+                .userId(userId)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .score(score)
+                .product(product)
+                .build();
+    }
+
+}
